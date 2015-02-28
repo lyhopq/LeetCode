89186b641b741959d8f272b0ed90f35827eb6a13
10125482 <liu.yuhe@zte.com.cn>
2015-02-28 14:55:27 +0800

    3-python

---
+++ python/longestSubstringWithoutRepeatingCharacters/longestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,37 @@
+def lengthOfLongestSubstring(str):
+    '''
+    Given a string, find the length of the longest substring without repeating characters. 
+    For example, the longest substring without repeating letters for "abcabcbb" is "abc", 
+    which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
+
+    >>> lengthOfLongestSubstring('abcabcbb')
+    3
+    >>> lengthOfLongestSubstring('bbbbb')
+    1
+    >>> lengthOfLongestSubstring('a')
+    1
+    '''
+    occur = {}
+    length = 0
+    longest = 0
+    startIndex = 0
+    for index, c in enumerate(str):
+        lastIndex = occur.get(c, None)
+        if lastIndex is None:
+            occur[c] = index
+            length += 1
+        else:
+            for i in range(startIndex, lastIndex):
+                del occur[str[i]]
+            if length > longest: longest = length
+            startIndex = lastIndex + 1
+            length = index - lastIndex
+            occur[c] = index
+
+    if length > longest: longest = length
+
+    return longest
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
